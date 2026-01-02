65dfb7ee8bebb9111d8be475be9d21f124a56041
Anandpandey400 <pandeyanand026@gmail.com>
2026-01-02 12:19:20 +0530

    User Data Entry

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ dotenv.config(); // MUST BE FIRST
 import app from "./app";
 console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
