98e46c7bbb0fecd56ca0cb0805601de97420538b
Anandpandey400 <pandeyanand026@gmail.com>
2026-01-02 12:55:55 +0530

    Error solving

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,10 @@
-import { Request } from "express";
+import "express";
 
 declare global {
     namespace Express {
         interface Request {
             user?: {
-                id: string;
+                id: number;
                 email: string;
             };
         }
